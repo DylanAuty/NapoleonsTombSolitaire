661108026a8101789504942572edb303da888836
Dylan Auty <dylan.auty@gmail.com>
2024-02-09 16:28:44 +0000

    Changed tack, not quite working but closer

--- main.py
+++ main.py
@@ -23,16 +23,35 @@ class NapoleonsTomb:
         
         """
         
-        # Set up deck
-        self.deck = list(range(13)) * 4  # Suits are irrelevant
-        random.shuffle(self.deck)
+        self.piles = [[] for i in range(12)]  # 12 piles: 4 for 7s, 1 for 6s, 4 for spares, 1 for spare 6s, 1 for discard, 1 for deck
+        # 0 1 2 3: 7s
+        # 4: 6s
+        # 5 6 7 8: spares
+        # 9: spare 6s
+        # 10: discard
+        # 11: deck
+        self.piles[11] = list(range(13)) * 4    # Generate deck, shuffle. Suits are irrelevant. Aces are 0s, KQJ are 10 11 and 12.
+        random.shuffle(self.piles[11])
+        self.hashmap = {    # What piles can card "key" go on? it can go on self.hashmap.get(key, []).
+            0:[5, 6, 7, 8],
+            1: [5, 6, 7, 8],
+            2: [5, 6, 7, 8],
+            3: [5, 6, 7, 8],
+            4: [5, 6, 7, 8],
+            5: [4, 9],
+            6: [0, 1, 2, 3],
+            7: [5, 6, 7, 8],
+            8: [5, 6, 7, 8],
+            9: [5, 6, 7, 8],
+            10: [5, 6, 7, 8],
+            11: [5, 6, 7, 8],
+            12: [5, 6, 7, 8],
+        }
 
-        self.sevens = [[], [], [], []]
-        self.sixes = []
-        self.spare_sixes = []
-        self.spares = []  # Max length == 4
-        self.discard = []
-
+        self.reverse_hashmap = {}    # I've occupied a pile. Where else in the hashmap do I need to update?
+        for k, v in self.hashmap.items():
+            for vv in v:
+                self.reverse_hashmap[vv] = self.reverse_hashmap.get(vv, []) + [k]
 
     def simulate_game(self) -> bool:
         """ Simulate one game. Return bool (True if win else False.) 
@@ -40,68 +59,71 @@ class NapoleonsTomb:
         Returns:
             bool: Whether a win occurred.
         """
-
-        while len(self.deck) > 0 or len(self.discard) > 0 or len(self.spares) > 0:
-            placed = False
-            # Fill empty spaces (spares, 7s, 6s).
-            self._fill_empty_slots()
-
-            # Try to place all spares.
-            while(not self._place_spares()):
+        num_moves = 0
+        while (any([len(self.piles[i]) > 0 for i in range(5, 12)])):    # Win condition: no placeable cards left.
+            num_moves += 1
+            print(f"Move {num_moves}: {[len(self.piles[i]) for i in range(len(self.piles))]}")
+            
+            # Check discard pile
+            if self._attempt_pile_placement(pile_idx=10):
                 continue
-                
-            
-            
-            # Try to place top of discard pile.
-            
-            # Try to place top card of deck. Succeeds unless deck is empty.
 
-            if not placed:
-                return False
+            # Check spares piles
+            if any([self._attempt_pile_placement(pile_idx=i) for i in range(5, 9)]):
+                continue
 
-        return True # Guaranteed win if deck, discard pile, and spares piles are all empty.
+            # Both failed, so attempt deck placement
+            if self._attempt_pile_placement(pile_idx=11):
+                continue
+            else:
+                if len(self.piles[11]) == 0:
+                    print(f"Failed, {num_moves} moves.")
+                    return False    # Discard is still full, but couldn't place it or anything else and there's nothing left to draw
+                else:
+                    print(f"Place a {self.piles[11][-1]} on the discard pile")
+                    self.piles[10].append(self.piles[11].pop())
 
-    
-    def _place_spares(self) -> bool:
-        """ Check spares piles for valid plays. 
+        print(f"Succeeded, {num_moves} moves")
+        return True
+
+
+    def _attempt_pile_placement(self, pile_idx: int) -> bool:
+        """ Try to place the top card of a given pile on one of the other piles.
 
         Returns:
-            bool: whether play was found.
+            bool: Whether placement was successful. 
         """
-        for card in self.spares:
-            if card <= 5:
-                if card == 5:
-                    if self.sixes == [] or self.sixes[-1] == 0:
-                        self.sixes.append(card)
-                        return True
-                    else:
-                        self.spare_sixes.append(card)
-                elif card == (self.sixes[-1] - 1):
-                    self.sixes.append(card)
-                    return True
-                
-            else:
-                # Handle 7-Ks
-                break
+        print(f"Placing card from pile: {pile_idx}")
+        if len(self.piles[pile_idx]) > 0:
+            card_val = self.piles[pile_idx][-1]
+            if len(self.hashmap.get(card_val, [])) > 0:
+                # If moving from a spares pile, cannot move to other spares piles.
+                if pile_idx in [5, 6, 7, 8]:
+                    for i, val in enumerate(self.hashmap[card_val]):
+                        if val <= 4 or val == 9:
+                            destination = self.hashmap[card_val].pop(i)
+                    return False    # No valid target pile found.
+                else:
+                    destination = self.hashmap[card_val].pop()
+                self.piles[destination].append(self.piles[pile_idx].pop())
+
+                print(f"Placed a {card_val} on pile {destination}")
+                if destination in [0, 1, 2, 3]: # 7s: pile grows upwards
+                    card_val += 1
+                    self.hashmap[card_val] = self.hashmap.get(card_val, []) + [destination]
+                elif destination == 4:  # 6s: pile growns downwards
+                    card_val = (card_val - 1) % 6
+                    self.hashmap[card_val] = self.hashmap.get(card_val, []) + [destination]
+                elif destination in [5, 6, 7, 8]:   # Spares pile is occupied now; remove as candidate everywhere.
+                    if len(self.reverse_hashmap[destination]) > 1:
+                        for d in self.reverse_hashmap[destination]:
+                            for i, val in enumerate(self.hashmap[d]):
+                                if val == destination:
+                                    self.hashmap[d].pop(i)
+
+                return True
+    
         return False
-
-    
-    def _fill_empty_slots(self) -> bool:
-        # Check and fill empty slots
-        while len(self.spares) != 4:
-            if len(self.discard) > 0:
-                self.spares.append(self.discard.pop(0))
-            elif len(self.deck) > 0:
-                self.spares.append(self.deck.pop(0))   # 6s will be handled when checking the board later
-            else:
-                sys.exit("Error: both discard and spares are empty, but trying to fill slots?")
-        if len(self.spare_sixes) > 0:
-            if self.sixes == [] or self.sixes[-1] == 0:
-                self.sixes.append(self.spare_sixes.pop(0))
-        
-
-
-    
 
 
 def main(args):
@@ -109,7 +131,12 @@ def main(args):
     for i in range(args.num_trials):
         game = NapoleonsTomb()
         result = game.simulate_game()
-
+        print(result)
+        if result:
+            wins += 1
+        else:
+            losses += 1
+    print(f"Wins: {wins}, Losses: {losses}")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
